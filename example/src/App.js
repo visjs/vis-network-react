3d09900124da5d1a793a04a30707826336d662c1
Yotam Berkowitz <yotamberk@gmail.com>
2020-03-20 13:01:29 +0200

    add initial dummy component wit hexample

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import VisNetworkReactComponent from './VisNetworkReactComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <VisNetworkReactComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class VisNetworkReactComponent extends Component {
+
+    render () {
+        console.log('aaaaaaaaaaaaaaa')
+        return (
+            <div>I am a dummy react npm module</div>
+        )
+
+    }
+
+}
