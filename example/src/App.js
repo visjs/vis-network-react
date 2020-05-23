7612445dfc392f4c50923df26c1bc69596838c83
yotamberk <yotamberk@gmail.com>
2020-05-23 23:15:10 +0300

    fix to use correct dataset

--- example/src/App.js
+++ example/src/App.js
@@ -1,114 +1,131 @@
-import React, { useCallback, useState } from 'react';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import "./App.css";
 
-import VisNetworkReactComponent from 'vis-network-react'
+import VisNetworkReactComponent from "vis-network-react";
 
 let defaultdata = {
   nodes: [
-    {id: 1, label: 'Node 1'},
-    {id: 2, label: 'Node 2'},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'},
+    { id: 1, label: "Node 1" },
+    { id: 2, label: "Node 2" },
+    { id: 3, label: "Node 3" },
+    { id: 4, label: "Node 4" },
+    { id: 5, label: "Node 5" },
   ],
   edges: [
-    {from: 1, to: 3},
-    {from: 1, to: 2},
-    {from: 2, to: 4},
-    {from: 2, to: 5},
-    {from: 3, to: 3}
+    { from: 1, to: 3 },
+    { from: 1, to: 2 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 3, to: 3 },
   ],
-}
+};
 
 let events = {
   click: function (params) {
-      params.event = "[original event]";
-      console.log('click event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
+    params.event = "[original event]";
+    console.log(
+      "click event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
+    );
   },
   doubleClick: function (params) {
-    console.log('doubleClick Event:', params);
+    console.log("doubleClick Event:", params);
     params.event = "[original event]";
   },
   oncontext: function (params) {
-    console.log('oncontext Event:', params);
+    console.log("oncontext Event:", params);
 
     params.event = "[original event]";
   },
   dragStart: function (params) {
-      // There's no point in displaying this event on screen, it gets immediately overwritten
-      params.event = "[original event]";
-      console.log('dragStart Event:', params);
-      console.log('dragStart event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
+    // There's no point in displaying this event on screen, it gets immediately overwritten
+    params.event = "[original event]";
+    console.log("dragStart Event:", params);
+    console.log(
+      "dragStart event, getNodeAt returns: " +
+        this.getNodeAt(params.pointer.DOM)
+    );
   },
   dragging: function (params) {
-      params.event = "[original event]";
+    params.event = "[original event]";
   },
   dragEnd: function (params) {
-      params.event = "[original event]";
-      console.log('dragEnd Event:', params);
-      console.log('dragEnd event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
+    params.event = "[original event]";
+    console.log("dragEnd Event:", params);
+    console.log(
+      "dragEnd event, getNodeAt returns: " + this.getNodeAt(params.pointer.DOM)
+    );
   },
   controlNodeDragging: function (params) {
-      params.event = "[original event]";
+    params.event = "[original event]";
   },
   controlNodeDragEnd: function (params) {
-      params.event = "[original event]";
-      console.log('controlNodeDragEnd Event:', params);
+    params.event = "[original event]";
+    console.log("controlNodeDragEnd Event:", params);
   },
-  zoom: function (params) {
-  },
-  showPopup: function (params) {
-  },
+  zoom: function (params) {},
+  showPopup: function (params) {},
   hidePopup: function () {
-      console.log('hidePopup Event');
+    console.log("hidePopup Event");
   },
   select: function (params) {
-      console.log('select Event:', params);
+    console.log("select Event:", params);
   },
   selectNode: function (params) {
-      console.log('selectNode Event:', params);
+    console.log("selectNode Event:", params);
   },
   selectEdge: function (params) {
-      console.log('selectEdge Event:', params);
+    console.log("selectEdge Event:", params);
   },
   deselectNode: function (params) {
-      console.log('deselectNode Event:', params);
+    console.log("deselectNode Event:", params);
   },
   deselectEdge: function (params) {
-      console.log('deselectEdge Event:', params);
+    console.log("deselectEdge Event:", params);
   },
   hoverNode: function (params) {
-      console.log('hoverNode Event:', params);
+    console.log("hoverNode Event:", params);
   },
   hoverEdge: function (params) {
-      console.log('hoverEdge Event:', params);
+    console.log("hoverEdge Event:", params);
   },
   blurNode: function (params) {
-      console.log('blurNode Event:', params);
+    console.log("blurNode Event:", params);
   },
   blurEdge: function (params) {
-      console.log('blurEdge Event:', params);
+    console.log("blurEdge Event:", params);
   },
-}
+};
 
 function App() {
-  const [ data, setData ] = useState(defaultdata)
+  const [data, setData] = useState(defaultdata);
+  const [networkNodes, setNetwortNodes] = useState([]);
 
   const handleAddNode = useCallback(() => {
-    const id = data.nodes.length + 1
-    setData({ 
-      ...data, 
-      nodes: [ 
-        ...data.nodes,     
-        { id, label: `Node ${id}`},
-      ] 
-    })
-  }, [ setData, data ])
+    const id = data.nodes.length + 1;
+    setData({
+      ...data,
+      nodes: [...data.nodes, { id, label: `Node ${id}` }],
+    });
+  }, [setData, data]);
+
+  const getNodes = useCallback((a) => {
+    setNetwortNodes(a);
+  }, []);
+
+  const handleGetNodes = useCallback(() => {
+    console.log(networkNodes);
+  }, [networkNodes]);
 
   return (
     <div className="App">
       <button onClick={handleAddNode}>add random node</button>
-      <VisNetworkReactComponent data={data} options={{}} events={events}/>
+      <button onClick={handleGetNodes}>get nodes</button>
+      <VisNetworkReactComponent
+        data={data}
+        options={{}}
+        events={events}
+        getNodes={getNodes}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,151 +1,132 @@
-import React, { Component } from "react";
-import defaultsDeep from "lodash/fp/defaultsDeep";
+/* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-filename-extension */
+import React, { useEffect, useRef } from "react";
 import isEqual from "lodash/isEqual";
 import differenceWith from "lodash/differenceWith";
-import { DataSet } from "vis-data";
+import { DataSet } from "vis-data/peer/esm/vis-data";
 import { Network } from "vis-network/peer/esm/vis-network";
-import { v4 as uuidv4 } from 'uuid';
 import PropTypes from "prop-types";
 
 import "vis-network/styles/vis-network.css";
 
-class Graph extends Component {
-  constructor(props) {
-    super(props);
-    const { identifier } = props;
-    this.updateGraph = this.updateGraph.bind(this);
-    this.state = {
-      identifier: identifier !== undefined ? identifier : uuidv4()
-    };
-    this.container = React.createRef();
-  }
-
-  componentDidMount() {
-    this.edges = new DataSet(this.props.data.edges);
-    this.nodes = new DataSet(this.props.data.nodes);
-
-    this.updateGraph();
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    let nodesChange = !isEqual(this.props.data.nodes, nextProps.data.nodes);
-    let edgesChange = !isEqual(this.props.data.edges, nextProps.data.edges);
-    let optionsChange = !isEqual(this.props.options, nextProps.options);
-    let eventsChange = !isEqual(this.props.events, nextProps.events);
-    console.log('should change?')
-    if (nodesChange) {
-      const idIsEqual = (n1, n2) => n1.id === n2.id;
-      const nodesRemoved = differenceWith(this.props.data.nodes, nextProps.data.nodes, idIsEqual);
-      const nodesAdded = differenceWith(nextProps.data.nodes, this.props.data.nodes, idIsEqual);
-      const nodesChanged = differenceWith(
-        differenceWith(nextProps.data.nodes, this.props.data.nodes, isEqual),
-        nodesAdded
-      );
-      this.patchNodes({ nodesRemoved, nodesAdded, nodesChanged });
-    }
-
-    if (edgesChange) {
-      const edgesRemoved = differenceWith(this.props.data.edges, nextProps.data.edges, isEqual);
-      const edgesAdded = differenceWith(nextProps.data.edges, this.props.data.edges, isEqual);
-      const edgesChanged = differenceWith(
-        differenceWith(nextProps.data.edges, this.props.data.edges, isEqual),
-        edgesAdded
-      );
-      this.patchEdges({ edgesRemoved, edgesAdded, edgesChanged });
-    }
-
-    if (optionsChange) {
-      this.Network.setOptions(nextProps.options);
-    }
-
-    if (eventsChange) {
-      let events = this.props.events || {};
-      for (let eventName of Object.keys(events)) this.Network.off(eventName, events[eventName]);
-      events = nextProps.events || {};
-      for (let eventName of Object.keys(events)) this.Network.on(eventName, events[eventName]);
-    }
-
-    return false;
-  }
-
-  componentDidUpdate() {
-    this.updateGraph();
-  }
-
-  patchEdges({ edgesRemoved, edgesAdded, edgesChanged }) {
-    this.edges.remove(edgesRemoved);
-    this.edges.add(edgesAdded);
-    this.edges.update(edgesChanged);
-  }
-
-  patchNodes({ nodesRemoved, nodesAdded, nodesChanged }) {
-    this.nodes.remove(nodesRemoved);
-    this.nodes.add(nodesAdded);
-    this.nodes.update(nodesChanged);
-  }
-
-  updateGraph() {
-    let defaultOptions = {
-      physics: {
-        stabilization: false
+const defaultOptions = {
+  physics: {
+    stabilization: false,
+  },
+  autoResize: false,
+  edges: {
+    smooth: false,
+    color: "#000000",
+    width: 0.5,
+    arrows: {
+      to: {
+        enabled: true,
+        scaleFactor: 0.5,
       },
-      autoResize: false,
-      edges: {
-        smooth: false,
-        color: "#000000",
-        width: 0.5,
-        arrows: {
-          to: {
-            enabled: true,
-            scaleFactor: 0.5
-          }
-        }
-      }
-    };
+    },
+  },
+};
 
-    // merge user provied options with our default ones
-    let options = defaultsDeep(defaultOptions, this.props.options);
-    this.Network = new Network(
-      this.container.current,
-      {
-        nodes: this.nodes,
-        edges: this.edges,
-      },
+const Graph = ({
+  data,
+  options = defaultOptions,
+  events = {},
+  style = { width: "100%", height: "100%" },
+  getNetwork,
+  getNodes,
+  getEdges,
+}) => {
+  const nodes = useRef(new DataSet(data.nodes));
+  const edges = useRef(new DataSet(data.edges));
+  const network = useRef(null);
+  const container = useRef(null);
+
+  useEffect(() => {
+    network.current = new Network(
+      container.current,
+      { nodes: nodes.current, edges: edges.current },
       options
     );
 
-    if (this.props.getNetwork) {
-      this.props.getNetwork(this.Network);
+    if (getNetwork) {
+      getNetwork(network.current);
     }
 
-    if (this.props.getNodes) {
-      this.props.getNodes(this.nodes);
+    if (getNodes) {
+      getNodes(nodes.current);
     }
 
-    if (this.props.getEdges) {
-      this.props.getEdges(this.edges);
+    if (getEdges) {
+      getEdges(edges.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    const nodesChange = !isEqual(nodes.current, data.nodes);
+    const edgesChange = !isEqual(edges.current, data.edges);
+
+    if (nodesChange) {
+      const idIsEqual = (n1, n2) => n1.id === n2.id;
+      const nodesRemoved = differenceWith(
+        nodes.current.get(),
+        data.nodes,
+        idIsEqual
+      );
+      const nodesAdded = differenceWith(
+        data.nodes,
+        nodes.current.get(),
+        idIsEqual
+      );
+      const nodesChanged = differenceWith(
+        differenceWith(data.nodes, nodes.current.get(), isEqual),
+        nodesAdded
+      );
+
+      nodes.current.remove(nodesRemoved);
+      nodes.current.add(nodesAdded);
+      nodes.current.update(nodesChanged);
     }
 
+    if (edgesChange) {
+      const edgesRemoved = differenceWith(
+        edges.current.get(),
+        data.edges,
+        isEqual
+      );
+      const edgesAdded = differenceWith(
+        data.edges,
+        edges.current.get(),
+        isEqual
+      );
+      const edgesChanged = differenceWith(
+        differenceWith(data.edges, edges.current.get(), isEqual),
+        edgesAdded
+      );
+      edges.current.remove(edgesRemoved);
+      edges.current.add(edgesAdded);
+      edges.current.update(edgesChanged);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    network.current.setOptions(options);
+  }, [options]);
+
+  useEffect(() => {
     // Add user provied events to network
-    let events = this.props.events || {};
-    for (let eventName of Object.keys(events)) {
-      this.Network.on(eventName, events[eventName]);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const eventName of Object.keys(events)) {
+      network.current.on(eventName, events[eventName]);
     }
-  }
+  }, [events]);
 
-  render() {
-    const { identifier } = this.state;
-    const { style } = this.props;[]
-    return <div id={identifier} style={style} ref={this.container}></div>
-  }
-}
-
-Graph.defaultProps = {
-  data: {},
-  style: { width: "100%", height: "100%" }
+  return <div ref={container} style={style} />;
 };
+
 Graph.propTypes = {
   data: PropTypes.object,
+  options: PropTypes.object,
+  events: PropTypes.object,
   style: PropTypes.object,
   getNetwork: PropTypes.func,
   getNodes: PropTypes.func,
